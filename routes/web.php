1d2c73aeca5c40bbd179f2c1e3197dcd3c6ce05d
Takao Nakada <acnakada@me.com>
2023-05-27 18:04:41 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//...
+
+use App\Http\Controllers\ImageController;
+
+//...
+
+Route::get('image-crop', [ImageController::class, "imageCrop"]);
+Route::post('image-crop', [ImageController::class, "imageCropPost"])->name("imageCrop");
